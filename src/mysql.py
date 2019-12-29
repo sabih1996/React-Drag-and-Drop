308deb1eb3d99035f20a14f1cf36e45c104157e0
SABIH <hafizsabhulhassan1996@gmail.com>
2019-12-29 23:37:09 +0500

    fourth commit

---
+++ src/mysql.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request 
+from flask_mysqldb import MySQL 
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config['MYSQL_USER'] = 'root1'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_tasks'
+
+mysql = MySQL(app)
+CORS(app)
+
+@app.route('/api/task', methods=['POST'])
+def add_task():
+    cur = mysql.connection.cursor()
+    tasks = request.get_json()['tasks']
+
+    cur.execute("INSERT INTO db_tasks.tasks (tasks) VALUES ('" + str(tasks) + "')")
+    mysql.connection.commit()
+    result = {'tasks':tasks}
+
+    return jsonify({"result": result})
